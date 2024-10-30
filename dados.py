60ba4a14d51b7440cd43bf12494f8b64dec26e82
teste <dalybie@gmail.com>
2024-10-30 14:39:09 -0300

    V0.1

---
+++ dados.py
@@ -0,0 +1,10 @@
+
+print(0b1110101) #binario
+print(0o123) #octal
+print(0x123,"\n") #hexadecimal
+
+print(True > False)
+print (True < False,"\n")
+
+print(3e6)
+print(3e-3)
---
+++ terceiro.py
@@ -0,0 +1,29 @@
+print('Meu nome é' , "Python." , end='\n')
+print('Monty Python.','\n' )
+
+print('Meu' ,'nome', 'é' ,'Python')
+print('Meu' ,'nome', 'é' ,'Python \n', sep='-')
+
+print('Meu' ,'nome', 'é',sep='-',end='*')
+print('Monty', 'Python.' , sep='*',end='\n')
+
+print('Programação','Essenciais', 'em' , sep='***',end='...')
+print('Python \n')
+
+print('    *     '*11)
+print('   * *    '*11)
+print('  *   *   '*11)
+print(" *     *  "*11)
+print('***   *** '*11)
+print('  *   *   '*11)
+print('  *   *   '*11)
+print('  *****   '*11)
+
+print('    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n')
+
+print('Eu gosto de Python')
+print('Eu gosto de \'Python\'')
+print('Eu gosto de "Python"')
+
+print()
+print('"Eu sou"\n""aprendiz de""\n\"\"\"Python\"\"\"')
